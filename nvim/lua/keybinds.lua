2ae5dc4338ad590d90875b692888c2e6810690ec
BlueWeabo <ilia.iliev@tuta.com>
2025-01-31 19:20:30 +0200

    nvim config

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,8 +1,42 @@
 local N = "n"
 local V = "v"
 local I = "i"
+local X = "x"
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set(N, "<leader>pv", vim.cmd.Ex)
---vim.keymap.set(N, "<leader><leader>)
+
+vim.keymap.set(V, "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set(V, "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set(N, "J", "mzJ`z")
+vim.keymap.set(N, "<C-d>", "<C-d>zz")
+vim.keymap.set(N, "<C-u>", "<C-u>zz")
+vim.keymap.set(N, "n", "nzzzv")
+vim.keymap.set(N, "N", "Nzzzv")
+
+-- greatest remap ever
+vim.keymap.set(X, "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({N, V}, "<leader>y", [["+y]])
+vim.keymap.set(N, "<leader>Y", [["+Y]])
+
+vim.keymap.set({N, V}, "<leader>d", [["_d]])
+
+vim.keymap.set(N, "Q", "<nop>")
+vim.keymap.set(N, "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set(N, "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set(N, "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set(N, "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set(N, "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set(N, "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set(N, "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set(N, "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set(N, "<leader><leader>", function()
+    vim.cmd("so")
+end)
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,37 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = "TSUpdate",
-    lazy = false,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter").setup({
-            ensure_installed = { "c", "lua", "rust", "bash", "java", "javascript", "typescript"},
-            sync_install = true,
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "vimdoc",
+                "javascript",
+                "typescript",
+                "c",
+                "lua",
+                "java",
+                "rust",
+            },
+            sync_install = false,
             auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
+            indent = {
+                enable = true
+            },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "markdown" },
+            },
         })
+
+        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        treesitter_parser_config.templ = {
+            install_info = {
+                url = "https://github.com/vrischmann/tree-sitter-templ.git",
+                files = { "src/parser.c", "src/scanner.c" },
+                branch = "master",
+            },
+        }
+
+        vim.treesitter.language.register("templ", "templ")
     end
 }
